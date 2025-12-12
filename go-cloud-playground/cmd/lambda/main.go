3980752e6346e878cea867009b9137eaf9b703dd
veenbreeze <mpambijevictor04@gmail.com>
2025-12-12 23:28:26 +0300

    let's do this

---
+++ go-cloud-playground/cmd/lambda/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to Go Cloud Playground!",
+		})
+	})
+
+	r.Run(":8080") // Runs on localhost:8080
+}
